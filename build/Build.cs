a5dd2b5589dd0872349e0e696fdd20e9f49b1434
Matthias Koch <ithrowexceptions@gmail.com>
2021-02-10 16:39:53 +0100

    Add Nuke.Enterprise project

--- build/Build.cs
+++ build/Build.cs
@@ -78,7 +78,7 @@ using static Nuke.Common.Tools.ReSharper.ReSharperTasks;
     OnPullRequestBranches = new[] { DevelopBranch },
     PublishArtifacts = false,
     InvokedTargets = new[] { nameof(Test), nameof(Pack) },
-    ImportSecrets = new[] { nameof(CodecovToken) })]
+    ImportSecrets = new[] { nameof(CodecovToken), nameof(EnterpriseAccessToken) })]
 [AppVeyor(
     AppVeyorImage.VisualStudio2019,
     AppVeyorImage.Ubuntu1804,
